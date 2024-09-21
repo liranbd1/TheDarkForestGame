4f104277ffd63da2214a8e1511eb35a14141dc6b
Liran <liranbd1@gmail.com>
2024-09-22 01:37:37 +0300

    commit

--- obj/Debug/net8.0/DarkForestGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/DarkForestGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DarkForestGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd0410c1c0a9672e4bfeea7cd8d29227eee115b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6282cacb7c9924a6331110f427d7ccb5a0443bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DarkForestGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DarkForestGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
